62997ada7b370f758abc8c1813fde4cdd94f9a9f
gabri-beep <biel790832157@gmail.com>
2025-05-03 01:01:33 -0300

    atualizandoEntity

--- ControleDespesas/src/main/java/com/example/ControleDespesas/Entity/Despesa.java
+++ ControleDespesas/src/main/java/com/example/ControleDespesas/Entity/Despesa.java
@@ -1,11 +1,17 @@
 package com.example.ControleDespesas.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Despesa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
