b05a4e354671ce881c7d215d7dd0e4f470fdd9bf
gabri-beep <biel790832157@gmail.com>
2025-05-02 23:18:39 -0300

    projeto criado

---
+++ ControleDespesas/src/main/java/com/example/ControleDespesas/ControleDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControleDespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDespesasApplication.class, args);
+	}
+
+}
---
+++ ControleDespesas/src/test/java/com/example/ControleDespesas/ControleDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleDespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
